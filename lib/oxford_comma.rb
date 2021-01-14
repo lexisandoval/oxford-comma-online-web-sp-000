b7e4b88d50bf9b88064af5877bdce556cab8ca12
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-14 16:39:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    x = array.pop
+    array << "and #{x}"
+    #array[array.length - 1] << " and "
+    array.join(", ")
+  end
 end
