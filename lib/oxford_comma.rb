e6bae370c5418bb244b99cca3122c3bbc8a4aa53
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-14 16:36:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   else
-    #array[array.length - 2] << " and "
+    array[array.length - 1] << " and "
     array.join(", ")
   end
 end
